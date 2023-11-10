0efe7a4cfd403cb677d7f95d47661017252ab024
Jesse Anderson <jeryanders@gmail.com>
2023-11-09 21:17:39 -0500

    fix: pipeline

--- dist/index.js
+++ dist/index.js
@@ -53600,16 +53600,16 @@ var lib_github = __nccwpck_require__(2867);
 
 async function printReport(report) {
   const summary = core.summary.addHeading("Summary");
-  summary.addRaw(`Actor: ${report.actor}`);
-  summary.addRaw(`SHA: ${report.sha}`);
-  summary.addRaw(`Branch: ${report.ref}`);
-  summary.addRaw(`Repository: ${report.repository.repo}`);
-  summary.addRaw(`Total Complexity: ${report.totalComplexity}`);
+  summary.addRaw(`Actor: ${report.actor}\n`);
+  summary.addRaw(`SHA: ${report.sha}\n`);
+  summary.addRaw(`Branch: ${report.ref}\n`);
+  summary.addRaw(`Repository: ${report.repository.repo}\n`);
+  summary.addRaw(`Total Complexity: ${report.totalComplexity}\n`);
   summary.addHeading("Complexity Report", 2);
   report.files.forEach((file) => {
-    summary.addHeading("File", file.file);
+    summary.addHeading(`File: ${file.file}\n`, 3);
     const mappedKeys = Object.keys(file.report).map(
-      (funcName) => file.report[funcName],
+      (funcName) => +file.report[funcName],
     );
     const maxComplexity = Math.max(mappedKeys);
     const totalComplexity = mappedKeys.reduce((prev, cur) => +prev + +cur);
--- src/report.js
+++ src/report.js
@@ -2,16 +2,16 @@ import * as core from "@actions/core";
 
 export async function printReport(report) {
   const summary = core.summary.addHeading("Summary");
-  summary.addRaw(`Actor: ${report.actor}`);
-  summary.addRaw(`SHA: ${report.sha}`);
-  summary.addRaw(`Branch: ${report.ref}`);
-  summary.addRaw(`Repository: ${report.repository.repo}`);
-  summary.addRaw(`Total Complexity: ${report.totalComplexity}`);
+  summary.addRaw(`Actor: ${report.actor}\n`);
+  summary.addRaw(`SHA: ${report.sha}\n`);
+  summary.addRaw(`Branch: ${report.ref}\n`);
+  summary.addRaw(`Repository: ${report.repository.repo}\n`);
+  summary.addRaw(`Total Complexity: ${report.totalComplexity}\n`);
   summary.addHeading("Complexity Report", 2);
   report.files.forEach((file) => {
-    summary.addHeading("File", file.file);
+    summary.addHeading(`File: ${file.file}\n`, 3);
     const mappedKeys = Object.keys(file.report).map(
-      (funcName) => file.report[funcName],
+      (funcName) => +file.report[funcName],
     );
     const maxComplexity = Math.max(mappedKeys);
     const totalComplexity = mappedKeys.reduce((prev, cur) => +prev + +cur);
