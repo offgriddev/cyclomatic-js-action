15cbe6a1bde4ca4a1db76ea88d7aac0bde91f7b3
Jesse Anderson <jeryanders@gmail.com>
2023-11-09 21:19:09 -0500

    fix: pipeline

--- src/report.js
+++ src/report.js
@@ -3,19 +3,26 @@ import * as core from "@actions/core";
 export async function printReport(report) {
   const summary = core.summary.addHeading("Summary");
   summary.addRaw(`Actor: ${report.actor}\n`);
+  summary.addBreak();
   summary.addRaw(`SHA: ${report.sha}\n`);
+  summary.addBreak();
   summary.addRaw(`Branch: ${report.ref}\n`);
+  summary.addBreak();
   summary.addRaw(`Repository: ${report.repository.repo}\n`);
+  summary.addBreak();
   summary.addRaw(`Total Complexity: ${report.totalComplexity}\n`);
+  summary.addBreak();
   summary.addHeading("Complexity Report", 2);
   report.files.forEach((file) => {
     summary.addHeading(`File: ${file.file}\n`, 3);
     const mappedKeys = Object.keys(file.report).map(
       (funcName) => +file.report[funcName],
     );
+    summary.addCodeBlock(mappedKeys);
     const maxComplexity = Math.max(mappedKeys);
     const totalComplexity = mappedKeys.reduce((prev, cur) => +prev + +cur);
     summary.addRaw(`Max Complexity: ${maxComplexity}`);
+    summary.addBreak();
     summary.addRaw(`Total Complexity: ${totalComplexity}`);
     summary.addSeparator();
   });
